4e26e9beb3eea34f1e4b0bc95f3946b67f88a9b4
CJ Williams <cj@cjwilliams.io>
2024-01-02 21:19:59 +0000

    Add Sentry error monitoring

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,34 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/static";
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cjwilliams.io',
-  integrations: [mdx(), sitemap(), tailwind({
-    applyBaseStyles: false
-  }), react()],
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    tailwind({
+      applyBaseStyles: false
+    }), 
+    react(), 
+    sentry({
+      dsn: "https://6ee6025b58e5a0cda45da4b80b6163cd@o4506503710703616.ingest.sentry.io/4506503712604160",
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
   output: "static",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     speedInsights: {
-      enabled: true,
+      enabled: true
     }
   })
 });
